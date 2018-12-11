c41cbc6ad136720196803c3653e077ee58199d29
mmueller23 <mmueller23@7QMHG52.ad.fh-bielefeld.de>
2018-12-11 10:31:01 +0100

    Gewinnbedinung eingefügt. Nullpointer

--- PraktikumPackage/src/SpielFeld2.java
+++ PraktikumPackage/src/SpielFeld2.java
@@ -48,18 +48,34 @@ public class SpielFeld2 {
 	}
 	
 	public boolean gewonnen() {
+		boolean gewonnen = false;
 		for(int zeile=0; zeile < anzahlZeilen; zeile++) {
 			for(int spalte=0; spalte < anzahlSpalten; spalte++) {
-				if(felder[spalte][zeile] != null) {
-					boolean farbe = felder[spalte][zeile].istSchwarz;
-					int counter = 1;
+				if(felder[spalte][zeile] != null && felder[spalte][zeile+1] != null && felder[spalte][zeile+2] != null && felder[spalte][zeile+3] != null || felder[spalte][zeile] != null && felder[spalte+1][zeile] != null && felder[spalte+2][zeile] != null && felder[spalte+3][zeile] != null ) {			
 					
-					if(felder[spalte][zeile+1].istSchwarz && felder[spalte][zeile+2].istSchwarz && felder[spalte][zeile+3].istSchwarz) {
+					if(felder[spalte][zeile].istSchwarz && felder[spalte][zeile+1].istSchwarz && felder[spalte][zeile+2].istSchwarz && felder[spalte][zeile+3].istSchwarz) {
 						StdOut.println("Das Spiel ist vorbei!");
+						gewonnen = true;
+						break;
+					}
+					else if(!felder[spalte][zeile].istSchwarz && !felder[spalte][zeile+1].istSchwarz && !felder[spalte][zeile+2].istSchwarz && !felder[spalte][zeile+3].istSchwarz){
+						StdOut.println("Das Spiel ist vorbei!");
+						gewonnen = true;
+						break;
+					}
+					else if(felder[spalte][zeile].istSchwarz && felder[spalte+1][zeile].istSchwarz && felder[spalte+2][zeile].istSchwarz && felder[spalte+3][zeile].istSchwarz){
+						StdOut.println("Das Spiel ist vorbei!");
+						gewonnen = true;
+						break;
+					}
+					else if(!felder[spalte][zeile].istSchwarz && !felder[spalte+1][zeile].istSchwarz && !felder[spalte+2][zeile].istSchwarz && !felder[spalte+3][zeile].istSchwarz){
+						StdOut.println("Das Spiel ist vorbei!");
+						gewonnen = true;
 						break;
 					}
 				}
 			}
 		}
+		return gewonnen;
 	}
 }
--- PraktikumPackage/src/VierGewinnt.java
+++ PraktikumPackage/src/VierGewinnt.java
@@ -1,20 +1,22 @@
 
 public class VierGewinnt {
-	
-	private static boolean farbe = false;
-
 	public static void main(String[] args) {
 		
 		SpielFeld2 spielFeld = new SpielFeld2();
+		boolean farbe = false;
+		boolean gewonnen = false;
 		
 		spielFeld.zeichne();
 		
-		while(true){
+		while(!gewonnen){
+			
 			
 			StdOut.println("In welche Spalte wollen Sie setzen?");
 			int spalte = StdIn.readInt();
+			
 			farbe = spielFeld.legeStein(spalte-1, farbe);
 			spielFeld.zeichne();
+			gewonnen = spielFeld.gewonnen();
 		}
 
 	}
